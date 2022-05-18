1dc3a2107f8a62a49162150243bb79fa3443130e
Brian Hernandez <hernandezbriannnn@gmail.com>
2022-05-17 23:18:51 -0700

    finished take-home

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-var button = document.querySelector('button');
+/* This is my first javascript comment */
+
+/*This is my second javascript comment
+  Bellow is the code to change the color of the box*/ 
+
+var button = document.querySelector ('button');
 
 var box = document.getElementById ('changeMe');
 
 button.onclick = function changeColor(){
-    box.style.background = 'blue';
+    box.style.background = 'Blue';
 }
+
+
+console.log("Brian Hernandez");
+console.log("My favorite food are tacos")
